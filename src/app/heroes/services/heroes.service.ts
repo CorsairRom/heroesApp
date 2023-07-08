abe9e17c6171b2c11590002bc461ea9c8bae8464
CorsairRom <ric.romero@duocuc.cl>
2023-07-07 22:48:14 -0400

    add: searchHero

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -4,7 +4,7 @@ import { Observable, catchError, of, pipe } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from 'src/environments/environments';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class HeroesService {
 
   private baseUrl: string = environments.baseUrl;
@@ -21,6 +21,10 @@ export class HeroesService {
       .pipe(
         catchError(error => of(undefined))
       );
+  }
+
+  getSuggestions( query:string ): Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${ query }&_limit=6`)
   }
 
 }
