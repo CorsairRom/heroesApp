b59ee9f2b92c2bc5526ce56cb4c028eb809a555b
CorsairRom <ric.romero@duocuc.cl>
2023-07-09 00:58:06 -0400

    add: hero Formgroup

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from "@angular/forms";
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-new-page',
@@ -8,9 +11,34 @@ import { Component } from '@angular/core';
 })
 export class NewPageComponent {
 
+  public heroForm = new FormGroup(
+    {
+      id:               new FormControl(''),
+      superhero:        new FormControl('', { nonNullable: true}),
+      publisher:        new FormControl<Publisher>(Publisher.DCComics),
+      alter_ego:        new FormControl(''),
+      first_appearance: new FormControl(''),
+      characters:       new FormControl(''),
+      alt_img:          new FormControl(''),
+    });
+
+
+
+
   public publishers = [
     {id: 'DC Comics', desc: 'DC - Comics'},
     {id: 'Marvel Comics', desc: 'Marvel - Comics'},
   ]
 
+  constructor (private heroService: HeroesService){}
+
+  get currentHero():Hero{
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+
+  onSubmit():void {
+    if(this.heroForm.invalid) return;
+
+  }
 }
